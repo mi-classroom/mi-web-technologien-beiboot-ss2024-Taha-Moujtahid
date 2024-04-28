44bf651d8bbf3421c572886a7fd9ea97276991bb
Taha <tsmoujtahid@outlook.de>
2024-04-28 22:15:19 +0200

    first issue

---
+++ backend/LongExposureBackend/Controller/VideoController.cs
@@ -0,0 +1,45 @@
+﻿using FFMpegCore;
+using FFMpegCore.Enums;
+using ImageMagick;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LongExposureBackend.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class VideoController
+{
+    private readonly IWebHostEnvironment _environment;
+    
+    public VideoController(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UploadVideo(IFormFile videoFile)
+    {
+        /*
+        string uploads = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+        string filePath = Path.Combine(uploads, videoFile.FileName);
+        using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+        {
+            await videoFile.CopyToAsync(fileStream);
+        }
+
+        string results = Path.Combine(Directory.GetCurrentDirectory(), "imgs");
+        await FFMpegArguments
+            .FromFileInput(filePath)
+            .OutputToFile(results+"/test%3d.png", true, options => options
+                .WithVideoCodec(VideoCodec.Png)
+                .WithFramerate(3))
+            .ProcessAsynchronously();
+*/
+        string results = Path.Combine(Directory.GetCurrentDirectory(), "imgs");
+        var resultImages = Directory.GetFiles(results, "test*.png").ToList();
+        using var images = new MagickImageCollection( resultImages.Select(path=> new MagickImage(path)).ToArray() );
+        images.Evaluate(EvaluateOperator.Median).Write(results+"/median.jpg");
+        
+        return new OkResult();
+    }
+}
